be834c7e509a55726111b5da3daf12457e91280d
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-13 13:05:23 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "my_private-ec2" {
+resource "aws_instance" "my_private_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.private_subnet_id
