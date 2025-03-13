458af657d3e8ce76100904c544990b0012b5ca59
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-13 12:17:57 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,9 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "ec2demo"
-
-  instance_type          = "t2.micro"
-resource "aws_instance" "p2_ec2" {
+resource "aws_instance" "my_private-ec2" {
   ami           = "ami-0d0f28110d16ee7d6"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.p2_private_subnet.id
-  vpc_security_group_ids = [aws_security_group.p2_ec2_sg.id]
+  instance_type = var.instance_type
+  subnet_id     = var.private_subnet_id
+
+  vpc_security_group_ids = [aws_security_group.my_ec2_sg.id]
 
 
   user_data = <<-EOF
@@ -20,7 +15,7 @@ resource "aws_instance" "p2_ec2" {
               EOF
 
   tags = {
-    Name = "PrivateEC2_P2"
+    Name = "Private_Instance"
   }
 }
 
