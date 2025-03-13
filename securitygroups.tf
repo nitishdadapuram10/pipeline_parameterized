2dcd4c4e16a96f9259d553e97a76c51f99ea63f6
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-13 13:07:33 -0500

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -38,7 +38,8 @@ resource "aws_security_group" "my_alb_sg" {
 
 resource "aws_security_group_rule" "allow_alb_to_ec2" {
   type                     = "ingress"
-  port                     = 80
+  from_port                = 80
+  to_port                  = 80
   protocol                 = "tcp"
   security_group_id        = aws_security_group.my_ec2_sg.id
   source_security_group_id = aws_security_group.my_alb_sg.id
